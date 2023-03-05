24306a44c5c4fd229dfbddf59629e75173972aec
Jody Bruchon <jody@jodybruchon.com>
2023-03-05 10:04:03 -0500

    Pull in main() skeleton from jodycalc project

--- jofito.c
+++ jofito.c
@@ -27,12 +27,32 @@
  #include <io.h>
 #endif
 
+#define MAX_LINE 512
+
+/* This holds the tokenized version of the code */
+char *tok_prog = NULL;
+
+
 void show_version(void)
 {
-	fprintf(stderr, "Jody Bruchon's File Tool %s (%s)\n", VER, VERDATE);
+	printf("Jody Bruchon's File Tool %s (%s)\n", VER, VERDATE);
+	printf("This program is pre-alpha and not currently useful.\n");
+	printf("Latest code available at https://github.com/jbruchon/jofito\n");
 	return;
 }
 
+
+void do_help(void)
+{
+	printf("There is no help text available yet.\n");
+	return;
+}
+
+
+int interpret_line(char *line, int len)
+{
+	return 0;
+}
 
 #ifdef UNICODE
 int wmain(int argc, wchar_t **wargv)
@@ -40,7 +60,28 @@ int wmain(int argc, wchar_t **wargv)
 int main(int argc, char **argv)
 #endif
 {
-	printf("Nothing is here yet!\n");
+	int len = 0;
+	char line[MAX_LINE];
+
+	line[MAX_LINE - 1] = '\0';
+
 	show_version();
-	exit(EXIT_FAILURE);
+
+	while (1) {
+		if (!fgets(line, MAX_LINE, stdin)) {
+			fprintf(stderr, "Error reading stdin\n");
+			exit(EXIT_FAILURE);
+		}
+		len = strlen(line);
+		/* Process line */
+		if (line[len-1] == '\n') {
+			len--;
+			line[len] = '\0';
+		}
+		if (len == 0) continue;
+		if (!strncmp(line, "quit", MAX_LINE) || !strncmp(line, "exit", MAX_LINE)) break;
+		else if (!strncmp(line, "help", MAX_LINE)) do_help();
+		else interpret_line(line, len);
+	}
+	exit(EXIT_SUCCESS);
 }
